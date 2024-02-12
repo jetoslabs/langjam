c72ea52835a76366ba0180ddb0d402e35a78c2ec
Anurag Jha <toanuragjha@gmail.com>
2024-02-12 11:01:32 -0800

    AUT-39: chore

--- tests/test_openai_utils.py
+++ tests/test_openai_utils.py
@@ -5,8 +5,7 @@ from typing import Optional
 import pytest
 from pydantic import BaseModel, Field
 
-from src.langjam.openai_utils import get_openai_function_defination
-from tests.chat_req import ChatReq
+from src.langjam.openai.openai_function.openai_function import OpenaiFunction
 from tests.expected import get_current_weather_expected
 
 
@@ -27,24 +26,11 @@ async def get_current_weather(get_current_weather_params: GetCurrentWeatherParam
 
 
 @pytest.mark.asyncio
-async def test_func_to_tool_happy_path_1():
+async def test_get_definition_happy_path_1():
     func = get_current_weather
-    resp = await get_openai_function_defination(func, "Get the current weather in a given location")
-    assert (
-            resp.model_dump_json(exclude_none=True).replace(" ", "") ==
-            json.dumps(get_current_weather_expected).replace(" ", "")
+    resp = await OpenaiFunction.get_defination(
+        func, "Get the current weather in a given location"
     )
-
-
-async def get_chat_req_test(chat_req: ChatReq):
-    return "get_chat_req_test"
-
-
-@pytest.mark.asyncio
-async def test_func_to_tool_happy_path_2():
-    func = get_chat_req_test
-    mjs = ChatReq.model_json_schema()
-    resp = await get_openai_function_defination(func, "Get the Openai chat")
     assert (
             resp.model_dump_json(exclude_none=True).replace(" ", "") ==
             json.dumps(get_current_weather_expected).replace(" ", "")
